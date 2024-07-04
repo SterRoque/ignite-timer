1ce504d4b08a19399f774f6ac0cec88a1eb758aa
Sterphane Roque <stermiss2@gmail.com>
2024-07-04 19:59:38 -0300

    Estrutura da aplicação - Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
